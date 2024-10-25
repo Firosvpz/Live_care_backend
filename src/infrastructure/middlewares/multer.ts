be6a5be1b779a260f3c736ea2dce568bab102060
firos <vpzfiroz@gmail.com>
2024-10-25 22:24:07 +0530

    multer update

--- src/infrastructure/middlewares/multer.ts
+++ src/infrastructure/middlewares/multer.ts
@@ -1,17 +1,6 @@
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const destPath = path.join(__dirname, "../public/images");
-    console.log("Saving file to:", destPath);
-    cb(null, destPath);
-  },
-  filename: (req, file, cb) => {
-    const filename = Date.now() + "-" + file.originalname;
-    console.log("Generated filename:", filename);
-    cb(null, filename);
-  },
-});
+// Configure multer to store files in memory
+const storage = multer.memoryStorage();
 
 export const uploadStorage = multer({ storage: storage });
